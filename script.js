0b57d67cdcfeefa9db1dfe84e91c1445504b90f6
sanaelakiranbabu-cmd <sanaelakiranbabu@gmail.com>
2026-03-10 18:18:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// smooth scrolling animation
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+anchor.addEventListener("click", function(e){
+
+e.preventDefault()
+
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+behavior:"smooth"
+})
+
+})
+
+})
